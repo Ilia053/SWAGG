0577b3960bddc735110e32108009279e979fe152
Ilia053 <nov-gruz@mail.ru>
2022-01-24 16:31:51 +0200

    login findBy

--- src/main/java/pageObject/LoginPage.java
+++ src/main/java/pageObject/LoginPage.java
@@ -1,4 +1,24 @@
 package pageObject;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(css = "[id='user-name']")
+    WebElement username ;
+    @FindBy(css = "[id='password']")
+    WebElement password;
+    @FindBy(css = "[id='login-button']")
+    WebElement buttonLogin;
+    @FindBy(css = "[data-test='error']")
+    WebElement textError;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 }
+
+
