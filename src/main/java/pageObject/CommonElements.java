ccddd42b1fd7044ac0f7d0e4f699297c72c1b1cc
Ilia053 <nov-gruz@mail.ru>
2022-02-03 13:21:05 +0200

    +++testng.xml works

--- src/main/java/pageObject/CommonElements.java
+++ src/main/java/pageObject/CommonElements.java
@@ -1,5 +1,19 @@
 package pageObject;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CommonElements {
+    @FindBy(css = "[id='react-burger-menu-btn']")
+    WebElement menuButton;
+    @FindBy(css = "[id='shopping_cart_container']")
+    WebElement cart;
+    @FindBy(css = "[class='social_twitter']")
+    WebElement twitter;
+    @FindBy(css = "[class='social_facebook']")
+    WebElement facebook;
+    @FindBy(css = "[class='social_linkedin']")
+    WebElement linkedin;
+
 
 }
